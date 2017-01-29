ff6b8e51b009c51a8cf80e25e049cfa514a445b0
Linken Dinh <linken.dinh@gmail.com>
2017-01-29 15:02:36 +0100

    wip

--- index.ts
+++ index.ts
@@ -1,14 +1,3 @@
-# typescript-debug-1
-tsconfig
-```json
-{
-  "compilerOptions": {
-    "noUnusedParameters": true
-  }
-}
-```
-
-```js
 export default (one, two, three, value, state, options) => {
   if (value % 1 !== 0) {
     return this.createError('number.round', { v: value }, state, options);
@@ -16,6 +5,3 @@ export default (one, two, three, value, state, options) => {
 
   return value;
 };
-```
-
-`one`, `two`, `three` is marked declared but never used.
